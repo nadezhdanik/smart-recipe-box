06e98e64b18c602bf948b3e5f57af91cfc3115f9
Nadezhda Memelova <nadezhdanik@gmail.com>
2026-01-27 10:33:15 +0200

    feat(module-2): dynamic text with interpolation

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,4 +8,6 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('My Recipe Box');
+  protected readonly tagline = signal('A place to save my favourite recipes');
+  protected readonly recipeCount = signal(0);
 }
