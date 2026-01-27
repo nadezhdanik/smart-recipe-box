aaee6c9b2ac02555aa457e2bf28ec6977b5eb3d3
Nadezhda Memelova <nadezhdanik@gmail.com>
2026-01-27 10:15:11 +0200

    feat(module-1): getting started

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('smart-recipe-box');
+  protected readonly title = signal('My Recipe Box');
 }
