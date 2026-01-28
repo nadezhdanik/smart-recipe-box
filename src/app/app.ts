9a8c1388d6456b95fd5616781d6bf077891011a3
Nadezhda Memelova <nadezhdanik@gmail.com>
2026-01-28 09:57:26 +0200

    feat(module-4): state management with writable signals part 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,6 @@
 import { Component, signal } from '@angular/core';
+import { MOCK_RECIPES } from './mock-recipes';
+import { RecipeModel } from './models';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +10,12 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('My Recipe Box');
-  protected readonly tagline = signal('A place to save my favourite recipes');
+  protected readonly activeRecipe = signal<RecipeModel>(MOCK_RECIPES[0]);
   protected readonly recipeCount = signal(0);
+  protected recipeName1 = MOCK_RECIPES[0].name;
+  protected recipeName2 = MOCK_RECIPES[1].name;
 
-  protected messages = {
-    add: 'A nice form to add a recipe will appear soon',
-    list: 'A recipe list will appear soon',
-  };
-
-  protected log(message: string): void {
-    console.log(message);
+  protected changeActiveRecipe(index: number): void {
+    this.activeRecipe.set(MOCK_RECIPES[index]);
   }
 }
