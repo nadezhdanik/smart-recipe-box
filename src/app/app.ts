c878b57f69bb95163c90efc35cd03d9b8a5adc05
Nadezhda Memelova <nadezhdanik@gmail.com>
2026-01-28 09:30:59 +0200

    feat(module-3): event listeners

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,4 +10,13 @@ export class App {
   protected readonly title = signal('My Recipe Box');
   protected readonly tagline = signal('A place to save my favourite recipes');
   protected readonly recipeCount = signal(0);
+
+  protected messages = {
+    add: 'A nice form to add a recipe will appear soon',
+    list: 'A recipe list will appear soon',
+  };
+
+  protected log(message: string): void {
+    console.log(message);
+  }
 }
